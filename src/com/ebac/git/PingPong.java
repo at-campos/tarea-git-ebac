071610454790c67baaace3cc22440c87876c6c80
at-campos <camposabril01@gmail.com>
2026-03-01 17:18:32 -0600

    Added lose condition

--- src/com/ebac/git/PingPong.java
+++ src/com/ebac/git/PingPong.java
@@ -6,20 +6,24 @@ public class PingPong {
     public void presentation(){
         System.out.println("Reglas del juego:");
         System.out.println("Introduce 'ping' para dar un raquetazo. La maquina responderá.");
-        System.out.println("Cualquier otra respuesta va a tomarse como un tiro errado.");
+        System.out.println("Cualquier otra respuesta va a tomarse como un tiro errado. 3 tiros y estás fuera.");
         System.out.println("Introduce 'salir' para finalizar la partida.");
     }
     public void newGame(){
         Scanner scan = new Scanner(System.in);
-        String str = scan.nextLine();
-        while (!str.toLowerCase().equals("salir")){
+        String str = "";
+        int missed = 0;
+
+        while (!str.toLowerCase().equals("salir") && missed < 3){
+            str = scan.nextLine();
             if (str.toLowerCase().equals("ping")){
                 System.out.println("¡Pong!");
             } else {
                 System.out.println("¡Errado!");
+                missed += 1;
             }
-            str = scan.nextLine();
         }
         System.out.println("¡Fin del juego!");
     }
+
 }
